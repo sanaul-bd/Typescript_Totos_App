129c8f22e47e54be242c2b525628a169b96a3af3
sanaul-bd <sanaullah.bd21@gmail.com>
2025-08-21 01:18:48 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+// dammi data 
+const todosData = [
+  {
+    id: "1",
+    title : "Todo Title 1"
+  },
+  {
+    id: "2",
+    title : "Todo Title 2"
+  },
+  {
+    id: "3",
+    title : "Todo Title 3"
+  },
+  {
+    id: "4",
+    title : "Todo Title 4"
+  },
+  {
+    id: "5",
+    title : "Todo Title 5"
+  },
+  {
+    id: "6",
+    title : "Todo Title 6"
+  },
+  {
+    id: "7",
+    title : "Todo Title 7"
+  },
+]
+
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
